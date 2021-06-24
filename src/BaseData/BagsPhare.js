ebe53f8e82a2ae2468fa1f2a1d7874daae9a3fbe
chafik <chafikdev92@gmail.com>
2021-06-24 16:23:36 +0200

    deploy

--- src/BaseData/BagsPhare.js
+++ src/BaseData/BagsPhare.js
@@ -3,23 +3,24 @@
     const BagsPhare = [
        
               {
-                "name": 'Mini zebra Talel ',
+                "name": 'MINI ZEBRA TALEL ',
                 "imageURL": "https://res.cloudinary.com/sharing-cloud/image/upload/v1624002809/zebre_blanc_xoib7j.png",
                 "price" : 350,
-                "Description" : "Genderless and timeless, the mini version of our triangle bags will be the perfect final touch for all your outfits.    nigdandayht occasions. ",
+                "Description" : "Genderless and timeless, the MINI version of our triangle bags will be the perfect final touch for all your outfits.    nigdandayht occasions. ",
                 "details" :  "Crafted in Italy  Embossed TALEL logo - Dimensions: height 10cm, depth 12cm, width 12cm - Composition 100% leather - Soft and removable nappa pouch in buttery lambskin - Adjustable and removable long leather strap - Comes with TALEL dustbag"},
                 {
-                  "name": 'Mini Purpule Talel ',
-                  "imageURL": "https://res.cloudinary.com/sharing-cloud/image/upload/v1624002809/violet_tfympe.png",
+                  "name": 'MINI FLOWER TALEL ',
+                  "imageURL": "https://res.cloudinary.com/sharing-cloud/image/upload/v1624002808/jungle_dgciuu.png",
                   "price" : 350,
-                  "Description" : "Genderless and timeless, the mini version of our triangle bags will be the perfect final touch for all your outfits.  day and night occasions. ",
+                  "Description" : "Genderless and timeless, the MINI version of our triangle bags will be the perfect final touch for all your outfits.  day and night occasions. ",
                   "details" :  "Crafted in Italy  Embossed TALEL logo - Dimensions: height 10cm, depth 12cm, width 12cm - Composition 100% leather - Soft and removable nappa pouch in buttery lambskin - Adjustable and removable long leather strap - Comes with TALEL dustbag"},
-                   {
-                      "name": 'Mini Orange Talel ',
-                      "imageURL": "https://res.cloudinary.com/sharing-cloud/image/upload/v1624002809/orange_mfx3m5.png",
-                      "price" : 350,
-                      "Description" : "Genderless and timeless, the mini version of our triangle bags will be the perfect final touch for all your outfits.  day and night occasions. ",
-                      "details" :  "Crafted in Italy  Embossed TALEL logo - Dimensions: height 10cm, depth 12cm, width 12cm - Composition 100% leather - Soft and removable nappa pouch in buttery lambskin - Adjustable and removable long leather strap - Comes with TALEL dustbag"},
+                  {
+                    "name": 'Mini Pink Talel ',
+                    "imageURL": "https://res.cloudinary.com/sharing-cloud/image/upload/v1624002809/rose_spq8mz.png",
+                    "price": 350,
+                    "Description": "Genderless and timeless, the mini version of our triangle bags will be the perfect final touch for all your outfits. These cute graphic bags exist in a variety of colors that will match every look. These unique bags are perfect for a casual day and night occasions. Carry it by the two handles or by the adjustable and removable leather strap",
+                    "details": "Crafted in Italy  Embossed TALEL logo - Dimensions: height 10cm, depth 12cm, width 12cm - Composition 100% leather - Soft and removable nappa pouch in buttery lambskin - Adjustable and removable long leather strap - Comes with TALEL dustbag"
+                  },
                    
         
     ]
--- src/components/CardSac.js
+++ src/components/CardSac.js
@@ -4,9 +4,7 @@ const CardSac = (props) => {
       <img className="AnimeImg" src={props.imageSac} alt="" />
 
       <div class="Sac-content">
-        <h3 className="AnimePrix"> {props.Prix + " EUR"} </h3>
-
-        <button
+      <button
           className="AnimeBTN"
           onClick={() => {
             props.setPanierSac(props.PanierSac + 1);
@@ -14,9 +12,23 @@ const CardSac = (props) => {
         >
           BUY ME
         </button>
+        <h3 className="AnimePrix"> {props.Prix + " EUR"} </h3>
+        
+
+        <p>"Description": "Genderless and timeless, the mini version of our triangle bags will be the perfect final touch for all your outfits. These cute graphic bags exist in a variety of colors that will match every look. These unique bags are perfect for a casual day and night occasions. Carry it by the two handles or by the adjustable and removable leather strap",</p>
+       
       </div>
 
       <h2> {props.Nom} </h2>
+      <button
+          className="AnimeBTN"
+          onClick={() => {
+            props.setPanierSac(props.PanierSac + 1);
+          }}
+        >
+          BUY ME
+        </button>
+    
     </div>
   );
 };
